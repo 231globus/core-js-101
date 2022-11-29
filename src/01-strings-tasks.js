76280a7ffb1189dac04e8b1a93357fd869408ded
231globus <231globus@gmail.com>
2022-11-30 00:34:22 +0300

    feat: omlemented functions

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -245,8 +245,15 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  const typeValue = typeof value;
+  if (typeValue === 'string') {
+    return true;
+  }
+  if (value instanceof String) {
+    return true;
+  }
+  return false;
 }
 
 
